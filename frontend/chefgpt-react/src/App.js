58d0d40fd3b9a61e6bcccc7e2662548b8bd91f20
Nick <nspeer252@gmail.com>
2023-01-21 13:51:45 -0500

    added example data

--- frontend/chefgpt-react/src/App.js
+++ frontend/chefgpt-react/src/App.js
@@ -14,8 +14,8 @@ const cookingMachine = createMachine({
   initial: "start",
   context: {
     title: "",
-    ingredients: [],
-    recipe: [],
+    ingredients: [{"text": "avocado"}, {"text": "tomato"}, {"text": "onion"}, {"text": "garlic"}, {"text": "cilantro"}, {"text": "lime"}, {"text": "salt"}, {"text": "pepper"}, {"text": "olive oil"}, {"text": "chicken breast"}, {"text": "corn tortillas"}, {"text": "cumin"}, {"text": "paprika"}, {"text": "oregano"}, {"text": "cayenne pepper"}, {"text": "chili powder"}, {"text": "black pepper"}, {"text": "salt"}, {"text": "garlic powder"}, {"text": "onion powder"}, {"text": "chicken broth"}, {"text": "tomato paste"}, {"text": "lime juice"}, {"text": "cilantro"}, {"text": "avocado"}, {"text": "tomato"}, {"text": "onion"}, {"text": "garlic"}, {"text": "cilantro"}, {"text": "lime"}, {"text": "salt"}, {"text": "pepper"}, {"text": "olive oil"}, {"text": "chicken breast"}, {"text": "corn tortillas"}, {"text": "cumin"}, {"text": "paprika"}, {"text": "oregano"}, {"text": "cayenne pepper"}, {"text": "chili powder"}, {"text": "black pepper"}, {"text": "salt"}, {"text": "garlic powder"}, {"text": "onion powder"}, {"text": "chicken broth"}, {"text": "tomato paste"}, {"text": "lime juice"}, {"text": "cilantro"}}],
+    recipe: [{"index": "1", "text": "make guac"}],
     done: false
   },
   states: {
@@ -37,6 +37,7 @@ const cookingMachine = createMachine({
         cook: "recipe",
         checkOffIngredient: {
           actions: assign((context, event) => {
+            console.log(context);
             const updatedIngredients = context.ingredients.map(ingredient => {
               if (ingredient.name === event.ingredient.name) {
                 return { ...ingredient, collected: true };
