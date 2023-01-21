fd946b6fb26ab86adae13ab9cc508099ee9d380c
George Lu <georgeludev@gmail.com>
2023-01-21 13:57:23 -0500

    clean up

--- backend/app.py
+++ backend/app.py
@@ -5,20 +5,12 @@ from dotenv import load_dotenv
 import openai
 
 app = FastAPI()
-
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
 
 @app.get("/chef/{query}")
 def chefgpt(query: str):
