ad38155380d1b9be249afb62ba89861cc22e00e6
Alex D <elxaitisaley@gmail.com>
2023-01-21 11:48:57 -0500

    feat: move generate recipe button to homepage

--- frontend/chefgpt-react/src/views/CelebrationPage.js
+++ frontend/chefgpt-react/src/views/CelebrationPage.js
@@ -6,15 +6,13 @@ export function CelebrationPage() {
         <div>
           <h1>Celebration Page!</h1>
           <button onClick={navigate}>Do another!</button>
-          <button onClick={generateRecipe}>Generate Recipe</button>
+          
         </div>
       );
       function navigate() {
         window.location.href = "/";
       }
-      function generateRecipe() {
-        console.log("Generating recipe...");
-      }
+      
 }
 
 export default CelebrationPage;
--- frontend/chefgpt-react/src/views/HomePage.js
+++ frontend/chefgpt-react/src/views/HomePage.js
@@ -6,10 +6,14 @@ export function HomePage() {
     <div>
       <h1>Home Page!</h1>
       <button onClick={handleClick}>Start Cooking</button>
+      <button onClick={generateRecipe}>Generate Recipe</button>
     </div>
   );
   function handleClick() {
     window.location.href = "/Cook";
+  }
+  function generateRecipe() {
+    console.log("Generating recipe...");
   }
 }
 
